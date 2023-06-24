1ceececccd93f1be59361ffa40c5a3d976905aad
Aphisit357 <137247053+Aphisit357@users.noreply.github.com>
2023-06-24 12:06:59 +0700

    kann

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,47 +2,47 @@ import Link from "next/link";
 import { BsArrowLeftCircle } from "react-icons/bs";
 export default function About() {
     return (
-        <div className="container mx-auto px-16">
+        <div className="container mx-auto md:px-16 px-5">
             <div className="py-10 flex justify-between">
             <Link className="text-white text-5xl "href="/"><BsArrowLeftCircle/></Link>
             <p className='kan text-4xl text-blue-800 font-bold'>SKILLS</p></div>
-            <div className="mt-6">
+            <div className="md:mt-6 mt-5">
                 <p className="text-white text-[20px]"> Word </p>
                 <div className="w-full bg-gray-200 rounded-full ">
                     <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full w-[65%]"> 65%</div>
                 </div>
             </div>
-            <div className="mt-6">
+            <div className="md:mt-6 mt-5">
                 <p className="text-white text-[20px]"> Powerpoint </p>
                 <div className="w-full bg-gray-200 rounded-full ">
                     <div className="bg-orange-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full w-[50%]"> 50%</div>
                 </div>
             </div>
-            <div className="mt-6">
+            <div className="md:mt-6 mt-5">
                 <p className="text-white text-[20px]"> Excel </p>
                 <div className="w-full bg-gray-200 rounded-full ">
                     <div className="bg-green-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full w-[40%]"> 40%</div>
                 </div>
             </div>
-            <div className="mt-6">
+            <div className="md:mt-6 mt-5">
                 <p className="text-white text-[20px]"> Visual Studio Code </p>
                 <div className="w-full bg-gray-200 rounded-full ">
                     <div className="bg-blue-400 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full w-[40%]"> 40%</div>
                 </div>
             </div>
-            <div className="mt-6">
+            <div className="md:mt-6 mt-5">
                 <p className="text-white text-[20px]"> CSS Style Text </p>
                 <div className="w-full bg-gray-200 rounded-full ">
                     <div className="bg-purple-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full w-[40%]"> 40%</div>
                 </div>
             </div>
-            <div className="mt-6">
+            <div className="md:mt-6 mt-5">
                 <p className="text-white text-[20px]"> Figma </p>
                 <div className="w-full bg-gray-200 rounded-full ">
                     <div className="bg-red-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full w-[65%]"> 65%</div>
                 </div>
             </div>
-            <div className="mt-6">
+            <div className="md:mt-6 mt-5">
                 <p className="text-white text-[20px]"> Adobe XD </p>
                 <div className="w-full bg-gray-200 rounded-full ">
                     <div className="bg-purple-800 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full w-[60%]"> 60%</div>
